f721e7d2bd87c8c5528b7ff889637811e32a9557
Suraj Kumar <surajkumar.tech32@gmail.com>
2026-09-04 00:17:17 +0530

    chore: initialize LogStream project

---
+++ backend/logstream-backend/src/main/java/logstream_backend/LogstreamBackendApplication.java
@@ -0,0 +1,13 @@
+package logstream_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogstreamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogstreamBackendApplication.class, args);
+	}
+
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div>
+      <h1>LogStream</h1>
+
+      <p>Distributed Log Analytics & Alerting Platform</p>
+
+      <input
+        type="text"
+        placeholder="Search logs..."
+      />
+
+      <h2>Log Results</h2>
+
+      <p>No logs available yet.</p>
+    </div>
+  );
+}
+
+export default App;
